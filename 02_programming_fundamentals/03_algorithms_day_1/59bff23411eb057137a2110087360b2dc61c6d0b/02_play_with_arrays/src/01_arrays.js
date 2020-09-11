3bf27fd49cf5e590d4a576245b84944ca0afb79e
LuggyG <loic.coryn@boulanger.com>
2020-09-11 11:05:37 +0200

    Ajout progress du jeudi

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/02_play_with_arrays/src/01_arrays.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/02_play_with_arrays/src/01_arrays.js
@@ -3,7 +3,7 @@
  */
 
 // Create an array called `digits` with the number from 0 to 9;
-
+const digit = [0,1,2,3,4,5,6,7,8,9];
 // create a constant called `first` for the first cell of your array;
 
 // create a constant called `last` for the last cell of your array;
