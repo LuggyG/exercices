5a78d140636ebd3bbcc0d4d887bb4ac5a539709e
LuggyG <loic.coryn@boulanger.com>
2020-09-14 09:02:01 +0200

    add day 2

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/02_digits_again.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/02_digits_again.js
@@ -1,3 +1,7 @@
 // ## Build an array
 //
 // Create the famous `digits` array using a `for-loop`.
+const digits = [""]
+
+for (let i = 0; i < digits.length; i++) {
+  console.log(someSports[i])
---
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/01_range_function/src/index.js
@@ -0,0 +1,20 @@
+function range(ascend, descend) {
+  // Code the function here.
+let newArray = [];
+if (ascend > descend){
+  while (ascend = descend){
+    ascend ++;
+    console.log(range)
+  }
+}
+};
+
+const ascArray = range(1, 4);
+// => [1, 2, 3, 4]
+
+const descArray = range(4, 1);
+// => [4, 3, 2, 1]
+// Do not remove last lines, it is for tests
+module.exports = range;
+
+// PAS FINIS 
