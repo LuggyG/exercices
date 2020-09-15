00034cd3ecfd5ca463681ec60b7a20a07b5a8136
LuggyG <loic.coryn@boulanger.com>
2020-09-15 09:44:35 +0200

    add day 4

---
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/01_create_callback/src/callback.js
@@ -0,0 +1,7 @@
+// code here
+
+function sayHello({ name } = player) {
+  console.log("Hello, " + name);
+}
+// leave the code below for tests to work
+module.exports = sayHello;
---
+++ 02_programming_fundamentals/05_algorithms_day_3/dae023a7ad3d6a1393ceb2733d443586c39347e3/02_use_callback/src/getInput.js
@@ -0,0 +1,13 @@
+// A function that takes two parameters, the second one being a callback function
+
+const getInput = {
+  user : null,
+  key : null,
+  function (callback){
+    return getInput;
+  },
+  // code the function here
+
+// Leave the line below for tests to work
+module.exports = getInput;
+  
